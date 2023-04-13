c5e9353d56056d7fe8166d8d7f4c306ed16e25f4
SudarshanKuncham545 <k.sudarshan5566@gmail.com>
2023-04-13 03:15:54 +0000

    Gallery App ReactJS CP12

---
+++ src/components/ThumbnailItem/index.js
@@ -0,0 +1,25 @@
+// Write your code here.
+import './index.css'
+
+const ThumbnailItem = props => {
+  const {thumbnailItem, changeImage} = props
+  const {imageUrl, thumbnailUrl, imageAltText, thumbnailAltText} = thumbnailItem
+
+  const onclickImage = () => {
+    changeImage(imageUrl, imageAltText)
+  }
+
+  return (
+    <li className="list-container">
+      <button type="button" className="button-style" onClick={onclickImage}>
+        <img
+          src={thumbnailUrl}
+          alt={thumbnailAltText}
+          className="thumbnail-image"
+        />
+      </button>
+    </li>
+  )
+}
+
+export default ThumbnailItem
